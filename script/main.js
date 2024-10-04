264785e9bcdf85cceea4dc0d141bcb8e1a33bc60
Sumit Patel <112008053+Sumit07Patel@users.noreply.github.com>
2024-10-05 02:08:02 +0530

    my first big project

---
+++ script/main.js
@@ -0,0 +1,32 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+        nav.classList.toggle('ham');
+    toggle.classList.toggle('click');
+    })
+ }
+ const mobileElement = document.querySelector('.mobile');
+const hamMenu = document.querySelector(".links-mobile");
+mobileElement.addEventListener('click', () => {
+    mobileElement.classList.toggle('ham');
+    hamMenu.classList.toggle('click');
+  });
+ 
+ showMenu('nav-toggle','nav-menu')
+
+ window.onscroll = function() {
+    let navbar = document.getElementById("navbarFun");
+    if (window.pageYOffset > 10) { 
+        navbar.classList.add("show");
+    } else {
+        navbar.classList.remove("show");
+    }
+};
